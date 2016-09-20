f812dfdda9bdf72f139da8efd97b69d7a142dc6d
srawan <shrestha.srawank@gmail.com>
2016-09-20 10:21:54 +0545

    Token Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenMismatchException)
+            return redirect()->back()->withWarning('Security token expired. Please retry.');
+
         return parent::render($request, $exception);
     }
 
---
+++ resources/views/provision/vps/partials/form-make.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: srawan
+ * Date: 9/18/2016
+ * Time: 3:06 PM
+ */
